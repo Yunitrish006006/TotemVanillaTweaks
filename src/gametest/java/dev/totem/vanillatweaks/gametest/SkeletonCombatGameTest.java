bbf605b633bd85bc6ce6a59a0f7618aaa3ce491d
阿昀 <yunthomas006@gmail.com>
2026-08-23 19:50:44 +0800

    test(skeleton): resolve entity types through registry

--- src/gametest/java/dev/totem/vanillatweaks/gametest/SkeletonCombatGameTest.java
+++ src/gametest/java/dev/totem/vanillatweaks/gametest/SkeletonCombatGameTest.java
@@ -3,6 +3,7 @@ package dev.totem.vanillatweaks.gametest;
 import net.fabricmc.fabric.api.gametest.v1.GameTest;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.gametest.framework.GameTestHelper;
+import net.minecraft.resources.Identifier;
 import net.minecraft.tags.EntityTypeTags;
 import net.minecraft.world.entity.EntityType;
 
@@ -10,18 +11,23 @@ import net.minecraft.world.entity.EntityType;
 public final class SkeletonCombatGameTest {
     @GameTest(maxTicks = 20)
     public void skeletonVariantsAreSensitiveToBaneOfArthropods(GameTestHelper helper) {
-        assertBaneSensitive(helper, EntityType.SKELETON, "skeleton");
-        assertBaneSensitive(helper, EntityType.STRAY, "stray");
-        assertBaneSensitive(helper, EntityType.BOGGED, "bogged");
-        assertBaneSensitive(helper, EntityType.WITHER_SKELETON, "wither skeleton");
-        assertBaneSensitive(helper, EntityType.SKELETON_HORSE, "skeleton horse");
-        assertBaneSensitive(helper, EntityType.PARCHED, "parched");
+        assertBaneSensitive(helper, "skeleton");
+        assertBaneSensitive(helper, "stray");
+        assertBaneSensitive(helper, "bogged");
+        assertBaneSensitive(helper, "wither_skeleton");
+        assertBaneSensitive(helper, "skeleton_horse");
+        assertBaneSensitive(helper, "parched");
         helper.succeed();
     }
 
-    private static void assertBaneSensitive(GameTestHelper helper, EntityType<?> type, String name) {
+    private static void assertBaneSensitive(GameTestHelper helper, String path) {
+        Identifier id = Identifier.fromNamespaceAndPath("minecraft", path);
+        EntityType<?> type = BuiltInRegistries.ENTITY_TYPE.getValue(id);
+        if (type == null) {
+            throw helper.assertionException("Missing entity type: " + id);
+        }
         if (!BuiltInRegistries.ENTITY_TYPE.wrapAsHolder(type).is(EntityTypeTags.SENSITIVE_TO_BANE_OF_ARTHROPODS)) {
-            throw helper.assertionException(name + " is not sensitive to Bane of Arthropods");
+            throw helper.assertionException(path + " is not sensitive to Bane of Arthropods");
         }
     }
 }
