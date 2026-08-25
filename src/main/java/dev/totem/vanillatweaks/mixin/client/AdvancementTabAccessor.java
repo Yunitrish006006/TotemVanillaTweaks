55464d18d6c171113ffe3eb0b5bbd8b8aadb2954
阿昀 <yunthomas006@gmail.com>
2026-08-25 21:04:26 +0800

    feat(observer): expose advancements tab pan

---
+++ src/main/java/dev/totem/vanillatweaks/mixin/client/AdvancementTabAccessor.java
@@ -0,0 +1,12 @@
+package dev.totem.vanillatweaks.mixin.client;
+
+import net.minecraft.client.gui.screens.advancements.AdvancementTab;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+/** Read-only access to the current advancements viewport pan. */
+@Mixin(AdvancementTab.class)
+public interface AdvancementTabAccessor {
+    @Accessor("scrollX") double totem$getScrollX();
+    @Accessor("scrollY") double totem$getScrollY();
+}
