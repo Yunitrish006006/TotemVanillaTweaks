08c715957747b41ebd1c15140d05a634101d79c1
阿昀 <yunthomas006@gmail.com>
2026-09-10 20:53:33 +0800

    refactor: route observer startup through subsystem facade

--- src/main/java/dev/totem/vanillatweaks/TotemVanillaTweaks.java
+++ src/main/java/dev/totem/vanillatweaks/TotemVanillaTweaks.java
@@ -4,7 +4,7 @@ import dev.totem.vanillatweaks.bookshelf.BookshelfInventoryRule;
 import dev.totem.vanillatweaks.content.VanillaTweaksContent;
 import dev.totem.vanillatweaks.manual.VanillaTweaksManual;
 import dev.totem.vanillatweaks.network.VanillaTweaksPayloadRegistration;
-import dev.totem.vanillatweaks.observer.ObserverSessionManager;
+import dev.totem.vanillatweaks.observer.ObserverServerRuntime;
 import dev.totem.vanillatweaks.skeleton.SkeletonAmmo;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
@@ -20,7 +20,7 @@ public final class TotemVanillaTweaks implements ModInitializer {
         VanillaTweaksManual.register();
         VanillaTweaksContent.register();
         VanillaTweaksPayloadRegistration.register();
-        ObserverSessionManager.register();
+        ObserverServerRuntime.register();
         BookshelfInventoryRule.register();
         SkeletonAmmo.register();
         LOGGER.info("TotemVanillaTweaks initialized");
